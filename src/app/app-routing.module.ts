52adf51d1e91cdea77ed936e5daea94dc8efbf6a
Alonso-20 <alonso_ca_30@hotmail.com>
2023-04-05 00:25:44 -0500

    login como ventana de inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,35 @@ import { DashboardComponent } from './modules/dashboard/pages/dashboard/dashboar
 
 const routes: Routes = [
   {
+<<<<<<< Updated upstream
     path:'DashboardClient',
     component:DashboardComponent,
     loadChildren:()=>import('src/app/modules/dashboard/dashboard.module').then(m=>m.DashboardModule)
+=======
+    path: '',
+    component: FreeviewComponent,
+    children: [
+      {path: '',  component: LoginComponent},
+      {path: 'about',  component: AboutComponent},
+      {path: 'login',  component: LoginComponent},
+      {path: 'register',  component: RegisterComponent},
+    ]
+  },
+  {
+    path: 'client',
+    component: ClientNavigationComponent,
+    children: [
+      {path: 'search', component: SearchCarComponent},
+      {path: 'my-car',  component: MyCarComponent},
+      {path: 'favourites', component: MyFavouritesComponent},
+      {path: 'rentals', component: MyRentalsComponent},
+      {path: 'reservations', component: MyReservationsComponent},
+      {path: 'subscription', component: SubscriptionComponent},
+      {path: 'profile', component: MyProfileComponent},
+      {path: 'search/car/:carId', component: CarComponent},
+      {path: 'profile/car/:carId', component: CarComponent}
+    ]
+>>>>>>> Stashed changes
   }
 ];
 
